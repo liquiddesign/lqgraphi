c2388e8c244e6c6d6271af67f6743b984cccd9eb
Petr Krehlik <73128826+petr6@users.noreply.github.com>
2022-11-04 14:27:21 +0100

    fixed remove bad method

--- src/Schema/BaseQuery.php
+++ src/Schema/BaseQuery.php
@@ -6,10 +6,4 @@ use Nette\Utils\Strings;
 
 abstract class BaseQuery extends BaseType
 {
-	public function getName(): string
-	{
-		$reflection = new \ReflectionClass($this->getClass());
-
-		return Strings::lower($reflection->getShortName());
-	}
 }
